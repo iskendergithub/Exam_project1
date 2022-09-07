f074df8eb2c304ddba18bde27ba16771029795be
iskendergithub <iskenmamatov@gmail.com>
2022-09-07 11:47:51 +0600

    added new files

---
+++ cafe/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import main_page, test_view
+
+
+urlpatterns = [
+    path('', main_page),
+    path('test/', test_view),
+]
--- cafe/views.py
+++ cafe/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+
+def main_page(request):
+    return HttpResponse("Hello, Honey !")
+
+
+def test_view(request):
+    number = 1000
+    return HttpResponse(number)
+
